092fd148bae5c19aee884801b2027d9a18009c4b
Safat99 <sarkersafat99@gmail.com>
2021-11-20 01:32:20 +0600

    different types of html tags are scraped

--- basic_scraping_part2.py
+++ basic_scraping_part2.py
@@ -1,11 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 
-link = "http://www.cuet.ac.bd/"
+# link = "http://www.cuet.ac.bd/"
+link = "https://www.kuet.ac.bd/department/CSE/index.php"
+# link = "https://www.cuet.ac.bd/dept/mie/notice-board"
 data = requests.get(link)
+print("request status code {}".format(data.status_code))
 
-if requests.status_code ==200:
-    print('request success!!')
+if data.status_code ==200:
+    print('request success!!\n')
+else: 
+    print('requets not found')
+    exit()
 
-    soup = BeautifulSoup(data.content, 'html.parser')
-    
+soup = BeautifulSoup(data.content, 'html.parser')
+# nav = soup.nav
+# print(nav)
+# print(type(nav))
+
+if type(nav) is not None:
+    for url in nav.find_all('a'):
+        print('links of navbars are:\n\n')
+        print(url.get('href'))
+print()
+
+body = soup.body 
+for paragraph in body.find_all('p'):
+    print(paragraph.text)
+
+# for links in body.find_all('a'):
+#     print(links.get('href'))
+
+for div in soup.find_all('div', class_='body'):
+    print(div.text)
+
+#for finding the nav for kuet website...
+for nav in soup.find_all('section', class_= 'menubar')
+    print(nav.text)
